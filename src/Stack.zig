387433d881fbcaa63f042c7d095eec559677a9cf
PedroRevesMD <pedroreves11@gmail.com>
2026-02-05 21:46:46 -0300

    test: initialize the Stack with empty items and length

--- src/Stack.zig
+++ src/Stack.zig
@@ -4,4 +4,10 @@ const Allocator = std.mem.Allocator;
 
 pub fn Stack(comptime _: type) type {
     return struct {};
+test "It should be able to initialize the stack" {
+    var stack = Stack(i32).init(testing.allocator);
+    defer stack.deinit();
+
+    try testing.expectEqual(@as(usize, 0), stack.len);
+    try testing.expectEqual(@as(usize, 0), stack.items.len);
 }
