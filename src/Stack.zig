0e8c941365bd30ba5dcbf8e6810b05d553b41529
PedroRevesMD <pedroreves11@gmail.com>
2026-02-05 21:47:03 -0300

    feat: initial Stack Implementation

--- src/Stack.zig
+++ src/Stack.zig
@@ -2,8 +2,31 @@ const std = @import("std");
 const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
-pub fn Stack(comptime _: type) type {
-    return struct {};
+pub fn Stack(comptime T: type) type {
+    return struct {
+        const Self = @This();
+        const GROWTH_FACTOR: usize = 2;
+        len: usize = 0,
+        items: []T = &.{},
+        allocator: std.mem.Allocator,
+        pub fn init(allocator: Allocator) Self {
+            return .{ .allocator = allocator };
+        }
+        pub fn deinit(self: *Self) void {
+            if (self.len == 0) return;
+            self.allocator.free(self.items.ptr[0..self.items.len]);
+            self.len = 0;
+            self.items = &.{};
+        }
+        // pub fn pop(self: *Self) ?T {}
+        // pub fn push(self: *Self, value: T) ?T {}
+        // pub fn isEmpty(self: *Self) bool {}
+        // pub fn isFull(self: *Self) bool {}
+        // fn grow(self: *Self) bool {}
+
+    };
+}
+
 test "It should be able to initialize the stack" {
     var stack = Stack(i32).init(testing.allocator);
     defer stack.deinit();
