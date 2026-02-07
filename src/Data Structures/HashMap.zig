cfd934fd459969d480ac453be1f7dfa36a27d071
PedroRevesMD <pedroreves11@gmail.com>
2026-02-07 14:56:02 -0300

    test: initialize and free HashMap

--- src/Data Structures/HashMap.zig
+++ src/Data Structures/HashMap.zig
@@ -12,3 +12,19 @@ pub fn Hashmap(comptime _: type) type {
         // pub fn deinit(se_f: *Self) void {}
     };
 }
+
+test "It should be able to initialize and deinitialize the HashMap" {
+    var hashmap = try Hashmap(i32, i32).init(testing.allocator, 0);
+    defer hashmap.deinit();
+
+    try testing.expectEqual(@as(usize, 0), hashmap.len);
+    try testing.expectEqual(@as(usize, 10), hashmap.buckets.len);
+}
+
+test "It should be able to initialize with custom capacity and deinitialize the HashMap" {
+    var hashmap = try Hashmap(i32, i32).init(testing.allocator, 5);
+    defer hashmap.deinit();
+
+    try testing.expectEqual(@as(usize, 0), hashmap.len);
+    try testing.expectEqual(@as(usize, 5), hashmap.buckets.len);
+}
